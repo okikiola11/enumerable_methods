fa868e7e300f4eba637fddd81469221b48795b3d
okikiola11 <okikiola.apelehin@gmail.com>
2020-05-01 09:55:50 +0000

    Add the other methods (my_map, my_inject) Configure linting errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/ModuleLength
 module Enumerable
   def my_each
     return enum_for(:my_each) unless block_given?
@@ -30,6 +31,7 @@ module Enumerable
     end
   end
 
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def my_all?(args = nil)
     condition = true
     if block_given?
@@ -38,14 +40,16 @@ module Enumerable
       my_each { |item| condition = false unless item.is_a?(args) }
     elsif args.is_a?(Regexp)
       my_each { |item| condition = false unless args.match?(item.to_s) }
-    elsif args.nil? == false
+    elsif !args.nil?
       my_each { |item| condition = false if item != args }
     else
       my_each { |item| condition = false unless item == true }
     end
     condition
   end
+  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def my_any?(args = nil)
     condition = false
     if block_given?
@@ -54,7 +58,7 @@ module Enumerable
       my_each { |item| condition = true unless item.is_a?(args) }
     elsif args.is_a?(Regexp)
       my_each { |item| condition = true unless args.match?(item.to_s) }
-    elsif args.nil? == false
+    elsif !args.nil?
       my_each { |item| condition = true if item != args }
     else
       my_each { |item| condition = true unless item == false }
@@ -62,15 +66,66 @@ module Enumerable
     condition
   end
 
+  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def my_none?(args = nil)
     condition = true
     if block_given?
       my_each { |item| condition = false if yield(item) == true }
-    elsif args.nil? == false
+    elsif !args.nil?
       my_each { |item| condition = false if item != args }
     else
       my_each { |item| condition = true unless item == false }
     end
     condition
   end
+  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+  def my_count(args = nil)
+    count = 0
+    if block_given?
+      my_each { |item| count += 1 if yield(item) }
+    elsif args
+      my_each { |item| count += 1 if item == args }
+    else
+      count = size
+    end
+    count
+  end
+
+  # rubocop:disable Performance/RedundantBlockCall
+  def my_map(&proc)
+    return enum_for(:my_map) unless block_given?
+
+    new_array = []
+    my_each do |item|
+      new_array << proc.call(item)
+    end
+    new_array
+  end
+  # rubocop:enable Performance/RedundantBlockCall
+
+  def my_inject(args = nil) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    reducer = args[0] if args[0].is_a?(Integer)
+
+    if block_given?
+      accumulator = 0
+      my_each { |item| accumulator = yield(accumulator, item) }
+      accumulator
+    elsif block_given? && args[0].is_a?(Integer)
+      accumulator = args[0]
+      my_each { |item| accumulator = yield(accumulator, item) }
+      accumulator
+    elsif args[0].is_a?(Symbol)
+      my_each { |item| reducer = reducer ? reducer.send(args[0], item) : item }
+      reducer
+    elsif args[0] && args[1].is_a?(Symbol)
+      my_each { |item| reducer = reducer ? reducer.send(args[1], item) : item }
+      reducer
+    end
+  end
 end
+
+def multiply_els(array)
+  array.my_inject(:*)
+end
+# rubocop:enable Metrics/ModuleLength

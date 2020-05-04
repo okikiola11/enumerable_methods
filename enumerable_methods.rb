c6d43609b8189f8f482b6b27da7b0977211b24f3
okikiola11 <okikiola.apelehin@gmail.com>
2020-05-04 15:14:40 +0000

    Fix code review

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -23,39 +23,30 @@ module Enumerable
     return enum_for(:my_select) unless block_given?
 
     result = []
-    i = 0
-    while i < size
-      result << self[i] if yield(self[i]) == true
-      i += 1
-    end
+    my_each { |item| result.push(item) if yield(item) }
+    result
   end
 
   def my_all?(args = nil)
     condition = true
     if block_given?
-      my_each { |item| condition = false if yield(item) == false }
-    elsif args.is_a?(Class)
-      my_each { |item| condition = false if item.is_a?(args) }
-    elsif args.is_a?(Regexp)
-      my_each { |item| condition = false if args.match?(item.to_s) }
-    elsif !args.nil?
-      my_each { |item| condition = false if item != args }
+      my_each { |item| condition = false if yield(item) }
+    elsif args.nil?
+      my_each { |item| condition = false if item == true }
     else
-      my_each { |item| condition = false if item }
+      my_each { |item| condition = false if item === args }
     end
     condition
   end
 
   def my_any?(args = nil)
     condition = false
-    if block_given?
-      my.each { |item| condition = true if yield(item) == true }
+    if args.nil?
+      my_each { |item| condition = true if yield(item) }
     elsif args.is_a?(Class)
       my_each { |item| condition = true if item.is_a?(args) }
     elsif args.is_a?(Regexp)
       my_each { |item| condition = true if args.match?(item.to_s) }
-    elsif args.nil?
-      my_each { |item| condition = true if item }
     else
       my_each { |item| condition = true if item == args }
     end
